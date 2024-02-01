e0934fd522183fa5717eb70f29ebd4bf405b982d
Mudassar Ali <mudassar.ali@vortx.com>
2024-02-01 20:02:17 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+
+// Define a custom attribute with AttributeUsage
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+class CustomAttribute : Attribute
+{
+    public CustomAttribute()
+    {
+        Console.WriteLine("CustomAttribute constructor called.");
+    }
+}
+
+// Define a class with a method marked with Conditional attribute
+class ConditionalClass
+{
+    [Conditional("DEBUG")]
+    public void DebugMethod()
+    {
+        Console.WriteLine("This method will only be called in DEBUG mode.");
+    }
+}
+
+class Program
+{
+    // Mark a method with Obsolete attribute
+    [Obsolete("This method is obsolete. Use NewMethod instead.")]
+    public static void OldMethod()
+    {
+        Console.WriteLine("This is the old method.");
+    }
+
+    public static void NewMethod()
+    {
+        Console.WriteLine("This is the new method.");
+    }
+
+    [Custom]
+    public static void MethodWithCustomAttribute()
+    {
+        Console.WriteLine("This method has a custom attribute.");
+    }
+
+    public static void Main(string[] args)
+    {
+        // Call the method marked with Conditional attribute
+        ConditionalClass conditionalClass = new ConditionalClass();
+        conditionalClass.DebugMethod();
+
+        // Call the method marked with Obsolete attribute
+        OldMethod(); // Produces a warning in IDE due to Obsolete attribute
+
+        // Call the method marked with Custom attribute
+        MethodWithCustomAttribute(); // Outputs message from CustomAttribute constructor
+
+        Console.ReadLine();
+    }
+}
